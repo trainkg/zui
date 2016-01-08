86d31fb04f931ca8f2389a9e64f41e2d7cc2509a
垂钓客 <peculiar.1@163.com>
2016-01-08 16:32:50 +0800

    布局面板初步我完成

--- src/main/webapp/zsq/core/panel.js
+++ src/main/webapp/zsq/core/panel.js
@@ -1,6 +1,4 @@
-/**
- * 面板,设定响应的布局~
- */
+//面板,设定响应的布局~
 define(['jquery','backbone','underscore',
         'text!./template/layout.html',
         'plugins/jlayout/jquery.sizes',
@@ -9,16 +7,45 @@ define(['jquery','backbone','underscore',
         'plugins/jlayout/jlayout.flow',
         'plugins/jlayout/jlayout.grid',
         'plugins/jlayout/jquery.jlayout',
-        'css!./css/layout'
+        'jquery-ui.min'
+        //'jquery.resizable'
+        //,'css!./css/layout'
+        ,'css!../css/jquery-ui.min'
  ],function($,Backbone,_,ftl) {
 	
   var defaults = {
 	  layout:{
 		resize : false,
-		type : 'border', //布局方案   border | flow | flexgrid | grid
-		vgap : 6,		 //垂直间距
-		hgap : 6		 //水平间距
-	  } 	//面板内容布局方案
+		type : 'border', // border | flow | flexgrid | grid
+		vgap : 4,		 
+		hgap : 4,		 
+		north:true,
+		west:true,
+		south:true,
+		east:true,
+		center:true,
+		northHt:50,
+		westWt:250,
+		southHt:50,
+		eastWt:250,
+		//border
+		resizable:{
+			south:{
+				resize:false
+			},
+			north:{
+				resize:false
+			},
+			west:{
+				resize:true
+			},
+			east:{
+				resize:true
+			}
+		},
+		resizeConfig:null, 	//JSON
+		resizeWin:true		//support change window size
+	  } 	
   };
     
    /*------BACKBONE-----*/
@@ -34,14 +61,53 @@ define(['jquery','backbone','underscore',
 		   	  case 'flexgrid': this.flexgridLayout();break;
 		   	  case 'grid'    : this.gridLayout();	 break;
 		   }
-		   this.$el.layout(this.context.layout);
+		   this.layout();
+		   this.initPlugins();
+		   
+		  
+	   },
+	   initPlugins:function(){
+		   //window resize
+		   var view = this,layout = this.context.layout;
+		   if(this.context.layout.resizeWin){
+			   $(window).resize(function(){
+				   view.layout();
+			   });
+		   }
+		   //panel resize
+		   var defConfig = {stop: function(){view.layout();view.$el.trigger('zsq.border.resize');}};
+		   if(this.context.layout.type === 'border'){
+			   if(layout.resizable.south.resize){
+				   var config = _.extend(layout.resizable.south,{handles: 'n',helper:'ui-resizable-helper-south'},defConfig);
+				   $('.south',this.$el).resizable(config); 
+			   }
+			   if(layout.resizable.north.resize){
+				   var config = _.extend(layout.resizable.north,{handles: 's',helper:'ui-resizable-helper-north'},defConfig);
+				   $('.north',this.$el).resizable(config); 
+			   }
+			   if(layout.resizable.east.resize){
+				   var config = _.extend(layout.resizable.east,{handles: 'w',helper:'ui-resizable-helper-east'},defConfig);
+				   $('.east',this.$el).resizable(config); 
+			   }
+			   if(layout.resizable.west.resize){
+				   var config = _.extend(layout.resizable.west,{handles: 'e',helper:'ui-resizable-helper-west'},defConfig);
+				   $('.west',this.$el).resizable(config); 
+			   }
+		   }
 	   },
 	   borderLayout:function(){
 		   this.$el.append(this.$template.find('.m-border-layout').children());
+		   this.$el.find('.west:first').css('width',this.context.layout.westWt+'px');
+		   this.$el.find('.north:first').css('height',this.context.layout.northHt+'px');
+		   this.$el.find('.east:first').css('width',this.context.layout.eastWt+'px');
+		   this.$el.find('.south:first').css('height',this.context.layout.southHt+'px');
 	   },
 	   flowLayout:function(){},
 	   flexgridLayout:function(){},
-	   gridLayout:function(){}
+	   gridLayout:function(){},
+	   layout:function(e){
+		   this.$el.layout(this.context.layout);
+	   }
    });
     
    return panel;
