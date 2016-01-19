8dc91c7a1c05d2d943dd4bf56ec353463925ed0d
垂钓客 <peculiar.1@163.com>
2016-01-19 18:03:51 +0800

    Signed-off-by: 垂钓å®? <peculiar.1@163.com>

--- src/main/webapp/zsq/core/dialog.js
+++ src/main/webapp/zsq/core/dialog.js
@@ -2,27 +2,27 @@
  * dialog
  */
 define(['./context','backbone','underscore','./window','./panel','./menuButton','./menu'
-        ,'text!app/ftl/tmenu.html'],function(ZSQ, Backbone,_,Window,Panel,Menubutton,Menu,template){
+        ,'text!app/ftl/tmenu.html','css!./css/dialog'],function(ZSQ, Backbone,_,Window,Panel,Menubutton,Menu,template){
 	
 	var $temples = $(template);
 	var defaults = {
 		menubutton:{
+			buttonCls:'btn-xs',
 			buttons:[{
 				name:'测试一号BUTTON',
+				cls:'btn-primary',
 				menu:new Menu({
 					template:$('#mm',$temples)[0].outerHTML,
 					onClick:function(item){
 						
 					}
 				})
-				,split:true
 			},
 			{
 				name:'测试二号BUTTON',
 				menu:new Menu({
 					template:$('#mm1',$temples)[0].outerHTML
 				})
-				,split:true
 			}]
 		}
 	};
@@ -34,15 +34,41 @@ define(['./context','backbone','underscore','./window','./panel','./menuButton',
 			//[].shift.apply(arguments)
 			var nomodify = {
 				onResize:function(width, height){
-					config.onResize.call(that._window, width, height);
+					that._resize(width,height);
+					if(config.onResize){
+						config.onResize.call(that._window, width, height);	
+					}
 				}
 			}
 			Window.prototype.initialize.call(this,_.extend(nomodify,defaults,config));
 			this.context.mbConfig = _.extend({},defaults.menubutton,config.menubutton)
-			this.menubutton = new Menubutton(this.context.mbConfig);
+			//hasMenu
+			if(this.context.mbConfig){
+				this.menubutton = new Menubutton(this.context.mbConfig);
+			}
 		},
 		render:function(){
 			Window.prototype.render.apply(this,arguments);
+			//dialog-toolbar
+			if(this.menubutton){
+				var $header = this.exec('header');
+				$header.after('<div class="dialog-toolbar"></div>');
+				var $tools = $('.dialog-toolbar',this.exec('window'));
+				var $body =  this.exec('body');
+				this.menubutton.setElement($tools).render();
+				this._resize();
+			}
+		},
+		//panel resize hander
+		_resize:function(width,height){
+			if(this.menubutton && this._window){
+				var $header = this.exec('header');
+				var $tools = $('.dialog-toolbar',this.exec('window'));
+				var $body =  this.exec('body');
+				console.log($body._outerHeight());
+				$body._outerHeight($body._outerHeight() - $tools._outerHeight());
+				console.log($body._outerHeight());
+			}
 		}
 	});
 	ZSQ.dialog = Dialog;
